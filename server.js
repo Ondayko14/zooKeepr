bb49694ed8e27d2e7db1e67a1f3db4f205fdb45d
Brandon O <b.ondayko@gmail.com>
2020-12-14 15:07:07 -0600

    added Heroku

--- server.js
+++ server.js
@@ -43,5 +43,5 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 app.listen(PORT, () => {
-    console,log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 })
