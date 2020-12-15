4e598aba377756ac1c5b713d53d426b0f3ad4eb0
Brandon O <b.ondayko@gmail.com>
2020-12-15 16:04:52 -0600

    server is now posting and validating

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 const {animals} = require('./data/animals.json');
 
 
@@ -40,6 +44,35 @@ function findById(id, animalsArray) {
     return result
 ;};
 
+function createNewAnimal(body, animalsArray) {
+    //main code
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, '/data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+    //return finished code
+    return animal;
+};
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string'){
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string'){
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if(req.query) {
@@ -56,6 +89,21 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
   });
+
+app.post('/api/animals', (req, res) => {
+    //sets the id
+    req.body.id = animals.length.toString();
+
+    //if any data in req.body is not properly formatted send 400
+    if(!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+
+        res.json(animal);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
