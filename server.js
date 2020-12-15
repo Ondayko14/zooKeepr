188c21d19b5fce354bbf427028b37b6955e918f7
Brandon O <b.ondayko@gmail.com>
2020-12-15 17:05:36 -0600

    Front End interaction is now hooked up

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 const {animals} = require('./data/animals.json');
 
 
@@ -107,6 +108,14 @@ app.post('/api/animals', (req, res) => {
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
 app.listen(PORT, () => {
